0f59198e3a42ccfdbec40464a2522b20bcf18b18
HowardWu5566 <ggg1237890@gmail.com>
2024-04-15 16:37:13 +0800

    feat: home page

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,18 @@ import express, { Request, Response } from 'express'
 import { engine } from 'express-handlebars'
 
 const app = express()
+
 app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
 app.set('views', './views')
+app.use(express.static('public'))
 
 app.get('/', (req: Request, res: Response) => {
   res.render('home')
+})
+
+app.get('/game', (req: Request, res: Response) => {
+  res.render('game')
 })
 
 app.listen(3000, () => {
